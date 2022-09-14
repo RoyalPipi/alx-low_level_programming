872f731de59d0eaa42b19f7d8c223bf84ced47d5
RoyalPipi <pobiekwuye@example.com>
2022-09-13 22:59:53 -0700

    corrections on 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* main - print putchar
-* Return; always 0
+* main
+*
+* Return: always 0
 */
 
 int main(void)
@@ -15,7 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n';
+	_putchar('\n');
 
 	return (0);
 
