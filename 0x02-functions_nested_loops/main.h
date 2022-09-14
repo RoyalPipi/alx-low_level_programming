4de2742fd931f964a3f5112a892a014aa1ef1117
RoyalPipi <pobiekwuye@example.com>
2022-09-13 21:44:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+int _putchar(char);
+void print _alphabet(void);
+void print_alphabet(void);
+void print_alphabetx10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
+
