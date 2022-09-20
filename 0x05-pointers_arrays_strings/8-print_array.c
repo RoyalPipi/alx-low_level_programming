eaa7ef3a604e7e084ea42fbdcb319982c636e475
RoyalPipi <pobiekwuye@example.com>
2022-09-19 20:58:39 -0700

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,7 +9,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i=0;
+	i = 0;
 	for (n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
@@ -20,4 +20,3 @@ void print_array(int *a, int n)
 	}
 	printf("\n");
 }
-
