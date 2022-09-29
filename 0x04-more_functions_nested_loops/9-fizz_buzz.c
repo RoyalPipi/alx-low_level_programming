c2dae979bf608b829bbdbba9fc69225b84481229
RoyalPipi <pobiekwuye@example.com>
2022-09-29 02:43:53 -0700

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,230 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100,
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");#include "main.h"
+#include <stdio.h>
+
+				/**
+				 *
+				
+				 main - prints the numbers from 1 to 100,
+				 but for multiples of three prints Fizz instead of the nuer
+				 and for the multiples of five prints Buzz
+				 Return: Always 0 (Success)
+				 /
+				 int main(void)
+				 {
+				 int i;
+
+				 for (i = 1; i <= 100; i++)++)
+				 {
+		
+		
+				 if (i % 3 == 0 && i % 5 != 0)
+				 {
+				 printf(" Fizz");
+				 } else if (i % 5 == 0 && i % 3 != 0)
+				 {
+				 printf(" Buzz");
+				 } else if (i % 3 == 0 && i % 5 == 0)
+				 {
+				 printf(" FizzBuzz")");#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100,
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+				int main(void)
+				{
+					int i;
+
+					for (i = 1; i <= 100; i++)
+++)
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+		printf(" Fizz");
+	} else if (i % 5 == 0 && i % 3 != 0)
+	{
+		printf(" Buzz");
+	} else if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf(" FizzBuz");#include "main.h"
+#include <stdio.h>
+
+			/**
+			 *
+		
+			 main - prints the numbers from 1 to 100,
+			 but for multiples of three prints Fizz instead of the ner
+			 and for the multiples of five prints Buzz
+			 Return: Always 0 (Success)
+			 /
+			 int main(void)
+			 {
+			 int i;
+
+			 for (i = 1; i <= 100; i++++)
+			 {
+	
+	
+			 if (i % 3 == 0 && i % 5 != 0)
+			 {
+			 printf(" Fizz");
+			 } else if (i % 5 == 0 && i % 3 != 0)
+			 {
+			 printf(" Buzz");
+			 } else if (i % 3 == 0 && i % 5 == 0)
+			 {
+			 printf(" FizzBuzz"");#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100,
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+			int main(void)
+			{
+				int i;
+
+				for (i = 1; i <= 100; i++)++)
+				{
+		
+		
+					if (i % 3 == 0 && i % 5 != 0)
+					{
+						printf(" Fizz");
+					} else if (i % 5 == 0 && i % 3 != 0)
+					{
+						printf(" Buzz");
+					} else if (i % 3 == 0 && i % 5 == 0)
+					{
+						printf(" FizzBuzz");");#include "main.h"
+#include <stdio.h>
+
+							/**
+							 *
+							 *
+
+							 main - prints the numbers from 1 to 100,
+							 but for multiples of three prints Fizz instead of the numbeer
+							 and for the multiples of five prints Buzz
+							 Return: Always 0 (Success)
+							 /
+							 int main(void)
+							 {
+							 int i;
+
+							 for (i = 1; i <= 100; i++)
+
+	++)
+
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+	printf(" Fizz");
+	} else if (i % 5 == 0 && i % 3 != 0)
+	{
+	printf(" Buzz");
+	} else if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf(" FizzBu");#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100,
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+							int main(void)
+							{
+								int i;
+
+								for (i = 1; i <= 100; i++)
+
+		++)
+		{
+
+
+			if (i % 3 == 0 && i % 5 != 0)
+			{
+				printf(" Fizz");
+			} else if (i % 5 == 0 && i % 3 != 0)
+			{
+				printf(" Buzz");
+			} else if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf(" FizzBuzz"");#include "main.h"
+#include <stdio.h>
+
+					/**
+					 *
+					 
+					 main - prints the numbers from 1 to 100,
+					 but for multiples of three prints Fizz instead of the numer
+					 and for the multiples of five prints Buzz
+					 Return: Always 0 (Success)
+					 /
+					 int main(void)
+					 {
+					 int i;
+
+					 for (i = 1; i <= 100; i++)
++
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+} else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzB");#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100,
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+					int main(void)
+					{
+						int i;
+
+						for (i = 1; i <= 100; i++)
+
+++)
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+		printf(" Fizz");
+	} else if (i % 5 == 0 && i % 3 != 0)
+	{
+		printf(" Buzz");
+	} else if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf(" FizzBuz");
---
+++ README.md
@@ -0,0 +1,1 @@
+the repo for hello world and variables task
